b3c130fd089859a7af5818504145d59821b59797
gusti-ngurah16 <ngurahigusti925@gmail.com>
2025-08-26 17:05:49 +0800

    menambahkan js

---
+++ script gallery.js
@@ -0,0 +1,21 @@
+const dessertTab = document.getElementById('dessert-tab');
+const foodTab = document.getElementById('food-tab');
+const drinkTab = document.getElementById('drink-tab');
+
+foodTab.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    window.location.href = 'Index Food Gallery.html';
+})
+
+drinkTab.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    window.location.href = 'Index Gallery.html';
+})
+
+dessertTab.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    window.location.href = 'Index Dessert Gallery.html';
+})
